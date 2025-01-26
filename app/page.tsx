3018414d7bc939f5175aa5c9da3bcc8805ba470a
Vitor Valente <dev.vitorlucas@gmail.com>
2025-01-25 23:56:32 -0300

    feat: adds the about and pricing page to the main project

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import { Header } from "@/components/header/header";
-import { Main } from "@/components/sessions/main";
+import { AboutSession } from "@/components/sessions/about";
+import { MainSession } from "@/components/sessions/main";
+import { PricingSession } from "@/components/sessions/pricing";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center flex-col px-8">
-      <Header />
+    <div className="w-full h-screen flex items-center flex-col">
 
       {/* Sessoes */}
-      <Main />
+      <MainSession />
+      <AboutSession />
+      <PricingSession />
     </div>
   );
 }
