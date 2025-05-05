90ba42aba55d099b06d540327109d58acbb64d11
Vitor Valente <ovitorvalente@gmail.com>
2025-05-04 21:44:22 -0300

    feat: adicionar hook useVisitorCounter para contagem de visitantes

---
+++ hooks/use-visitor-counter.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export function useVisitorCounter() {
+  const [visitorCount, setVisitorCount] = useState(0);
+
+  useEffect(() => {
+    const randomVisitors = Math.floor(Math.random() * 20) + 3023;
+    setVisitorCount(randomVisitors);
+
+    const interval = setInterval(() => {
+      setVisitorCount((prev) => prev + 1);
+    }, 8000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return visitorCount;
+}
